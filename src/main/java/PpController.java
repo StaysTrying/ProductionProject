c0ef41e43912da6ce816a4c1fab3cd7df513daa3
Tyler <tyler.krawec@gmail.com>
2020-09-20 16:58:16 -0400

    Alpha

--- src/main/java/PpController.java
+++ src/main/java/PpController.java
@@ -63,11 +63,51 @@ public class PpController {
 
         cmbQuantity.getSelectionModel().selectFirst();
 
-        for( int count = 1; count <=10; count++ ){
+        for (int count = 1; count <= 10; count++) {
             cmbQuantity.getItems().add(String.valueOf(count));
         }
     }
 
+    public void printProd() {
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/ProdProj";
+
+        //  Database credentials
+        final String USER = "";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM Product";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+                System.out.println(rs.getString(4));
+
+            }
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void updateProduct() {
         final String JDBC_DRIVER = "org.h2.Driver";
@@ -75,55 +115,41 @@ public class PpController {
 
 
         //  Database credentials
-
         final String USER = "";
         final String PASS = "";
 
         Connection conn = null;
         Statement stmt = null;
 
-
         String name = prodName.getText();
         String manufacturer = prodManuf.getText();
 
         try {
-
             // STEP 1: Register JDBC driver
-
             Class.forName(JDBC_DRIVER);
 
-
             //STEP 2: Open a connection
-
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-
             //STEP 3: Execute a query
-
             stmt = conn.createStatement();
-
 
             String sql = "INSERT INTO Product(type, manufacturer, name) " +
                     "VALUES ( 'Audio', '" + manufacturer + "' , '" + name + "' )";
 
-
             stmt.executeUpdate(sql);
 
-
-            System.out.println("Select * FROM Products");
+            printProd();
 
             // STEP 4: Clean-up environment
-
             stmt.close();
             conn.close();
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
 
-
         } catch (SQLException e) {
             e.printStackTrace();
-
         }
     }
 }
