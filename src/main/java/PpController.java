f77dbdd58040898276048dda80e04ca8ca05ba70
Tyler <tyler.krawec@gmail.com>
2020-09-20 15:55:00 -0400

    Alpha

--- src/main/java/PpController.java
+++ src/main/java/PpController.java
@@ -1,13 +1,36 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.event.ActionEvent;
-import java.sql.*;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 
 public class PpController {
 
     @FXML
     private Tab tbProductionLine;
+
+    @FXML
+    private GridPane prodLineInput;
+
+    @FXML
+    private TextField prodName;
+
+    @FXML
+    private TextField prodManuf;
+
+    @FXML
+    private ComboBox<String> itemType;
 
     @FXML
     private Button buttonAddItem;
@@ -19,11 +42,14 @@ public class PpController {
     private Button buttonRecProd;
 
     @FXML
+    private ComboBox<String> cmbQuantity;
+
+    @FXML
     private Tab tbProductionLog;
 
     @FXML
     void addItem(ActionEvent event) {
-
+        updateProduct();
     }
 
     @FXML
@@ -32,6 +58,66 @@ public class PpController {
     }
 
     public void initialize() {
+        for( int count = 1; count <=10; count++ ){
+            cmbQuantity.getItems().add(String.valueOf(count));
+        }
     }
 
+
+    public void updateProduct() {
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/ProdProj";
+
+
+        //  Database credentials
+
+        final String USER = "";
+        final String PASS = "";
+
+        Connection conn = null;
+        Statement stmt = null;
+
+
+        String name = prodName.getText();
+        String manufacturer = prodManuf.getText();
+
+        try {
+
+            // STEP 1: Register JDBC driver
+
+            Class.forName(JDBC_DRIVER);
+
+
+            //STEP 2: Open a connection
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+
+            //STEP 3: Execute a query
+
+            stmt = conn.createStatement();
+
+
+            String sql = "INSERT INTO Product(type, manufacturer, name) " +
+                    "VALUES ( 'Audio', '" + manufacturer + "' , '" + name + "' )";
+
+
+            stmt.executeUpdate(sql);
+
+
+            // STEP 4: Clean-up environment
+
+            stmt.close();
+            conn.close();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
 }
+
